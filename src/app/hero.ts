352799957b3e3c1610e5d98a129c523b1edcbdf8
RoyDevTEAM <roidercito12345@gmail.com>
2024-12-21 11:49:51 -0400

    Modificando heroes para proyecto

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,4 +1,7 @@
+
+// La interfaz 'Hero' define la estructura de los objetos héroes
 export interface Hero {
-  id: number;
-  name: string;
+  id: number; // ID único del héroe
+  name: string; // Nombre del héroe
+  img: string; // Ruta de la imagen del héroe
 }
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,15 +1,17 @@
-//Este código representa el "cap dos" del tutorial de "Heroes" en Angular, donde se define un arreglo de héroes en un archivo llamado 'hero.ts'
+// Este archivo define un arreglo de héroes para el tutorial de "Heroes" en Angular (Capítulo 2)
 import { Hero } from './hero'; // Importa la clase Hero desde el archivo 'hero.ts'
 
-export const HEROES: Hero[] = [ // Define una constante 'HEROES' que es un arreglo de objetos de tipo Hero
-  { id: 11, name: 'Lionel Messi' }, // Cada objeto representa un héroe con un ID y un nombre
-  { id: 12, name: 'Xavi Hernandez' },
-  { id: 13, name: 'Andres Iniesta' },
-  { id: 14, name: 'Carles Puyol' },
-  { id: 15, name: 'Ronaldinho' },
-  { id: 16, name: 'Gerard Pique' },
-  { id: 17, name: 'Sergio Busquets' },
-  { id: 18, name: 'Luis Suarez' },
-  { id: 19, name: 'Neymar Jr.' },
-  { id: 20, name: 'Javier Mascherano' },
+// Define una constante 'HEROES' que es un arreglo de objetos de tipo Hero
+// Cada héroe tiene un ID, un nombre y una URL de imagen asociada
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Lionel Messi', img: 'assets/img/messi.webp' },
+  { id: 12, name: 'Xavi Hernandez', img: 'assets/img/xavi.webp' },
+  { id: 13, name: 'Andres Iniesta', img: 'assets/images/iniesta.jpg' },
+  { id: 14, name: 'Carles Puyol', img: 'assets/img/puyol.jpg' },
+  { id: 15, name: 'Ronaldinho', img: 'assets/img/ronaldinho.jpg' },
+  { id: 16, name: 'Gerard Pique', img: 'assets/images/pique.jpg' },
+  { id: 17, name: 'Sergio Busquets', img: 'assets/images/busquets.jpg' },
+  { id: 18, name: 'Luis Suarez', img: 'assets/images/suarez.jpg' },
+  { id: 19, name: 'Neymar Jr.', img: 'assets/images/neymar.jpg' },
+  { id: 20, name: 'Javier Mascherano', img: 'assets/images/mascherano.jpg' },
 ];
